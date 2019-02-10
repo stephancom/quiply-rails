1a9d58defa1caa9de4d3977a6452c3663da388d7
stephan.com <stephan@stephan.com>
2019-02-09 21:47:23 -0500

    validations/relations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :user, primary_key: :old_id, inverse_of: :orders, required: true
+  validates :order_num, uniqueness: { scope: :user_id }
+  validates :old_id, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :orders, primary_key: :old_id, dependent: :destroy, inverse_of: :user
+  validates :old_id, uniqueness: true
 end
